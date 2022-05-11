062a41fa39a944639ac169fc0026645711f29bc7
Yifshao <80507783+Yifshao@users.noreply.github.com>
2022-05-10 23:34:04 -0400

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,62 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy contracts here:
+  const NFT_CSGO = await ethers.getContractFactory("NFT_CSGO");
+  const NFT_deploy = await NFT_CSGO.deploy();
+  const Market = await ethers.getContractFactory("HighlightMarket");
+  const Market_deploy = await Market.deploy(1);
+
+  console.log("NFT contract address", NFT_deploy.address);
+  console.log("Market contract address", Market_deploy.address);
+  
+  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
+  saveFrontendFiles(NFT_deploy,"NFT_CSGO");
+  saveFrontendFiles(Market_deploy,"HighlightMarket");
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../src/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
