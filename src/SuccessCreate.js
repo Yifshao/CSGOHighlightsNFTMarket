01feaeb811c43e4b3d7809102aadd98f352c006a
Yifshao <80507783+Yifshao@users.noreply.github.com>
2022-05-11 17:51:11 -0400

    Add files via upload

--- src/SuccessCreate.js
+++ src/SuccessCreate.js
@@ -5,7 +5,7 @@ const SuccessCreate = () =>{
 return(
 <main style={{padding:"1rem 0"}}>
     <h2 bsSize="lg">Thank you for creating/selling your NFT</h2>
-    <p>Please confirm the transformations in your wallet. </p>
+    <p>Please confirm the transactions in your wallet. </p>
     <p>Go to the Home page to see your NFT.</p>
     <Button varient='primary' size='lg' as={Link} to="/">
         Home
